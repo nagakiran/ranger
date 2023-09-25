4d266e52abe72fe1c7a43fc68a0ef2151d250138
nagakiran <nagakiran@juniper.net>
2023-09-25 11:19:54 +0530

    fix: Enabled inline edit for feh fix: Added csv formatter for preview

--- ranger/data/scope.sh
+++ ranger/data/scope.sh
@@ -130,6 +130,10 @@ handle_extension() {
             mediainfo "${FILE_PATH}" && exit 5
             exiftool "${FILE_PATH}" && exit 5
             ;; # Continue with next handler on failure
+
+        csv)
+            table "${FILE_PATH}" && exit 5
+            ;; # Continue with next handler on failure
     esac
 }
 
